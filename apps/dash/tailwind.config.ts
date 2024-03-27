39bc4b52494de51ca69ed67ff758de77bc903472
Abdullah Elsayed <11673834+abdllahdev@users.noreply.github.com>
2024-03-27 13:34:32 +0200

    feat: init dash

---
+++ apps/dash/tailwind.config.ts
@@ -0,0 +1,20 @@
+// tailwind config is required for editor support
+
+import tailwindConfig from "@repo/ui/tailwind-config";
+
+import type { Config } from "tailwindcss";
+
+const config: Pick<Config, "content" | "presets" | "theme"> = {
+  content: ["./src/app/**/*.tsx"],
+  presets: [tailwindConfig],
+  theme: {
+    extend: {
+      backgroundImage: {
+        "glow-conic":
+          "conic-gradient(from 180deg at 50% 50%, #2a8af6 0deg, #a853ba 180deg, #e92a67 360deg)",
+      },
+    },
+  },
+};
+
+export default config;
