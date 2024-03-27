92cf6526268550103c530aac5c141d8c3adafd69
Abdullah Elsayed <11673834+abdllahdev@users.noreply.github.com>
2024-03-27 13:41:07 +0200

    chore(ui): run formatter

--- packages/ui/src/components/ui/form.tsx
+++ packages/ui/src/components/ui/form.tsx
@@ -3,7 +3,6 @@ import type * as LabelPrimitive from "@radix-ui/react-label";
 import { Slot } from "@radix-ui/react-slot";
 import type { ControllerProps, FieldPath, FieldValues } from "react-hook-form";
 import { Controller, FormProvider, useFormContext } from "react-hook-form";
-
 import { cn } from "@/lib/utils";
 import { Label } from "@/components/ui/label";
 
--- packages/ui/src/components/ui/input-otp.tsx
+++ packages/ui/src/components/ui/input-otp.tsx
@@ -3,7 +3,6 @@
 import * as React from "react";
 import { DashIcon } from "@radix-ui/react-icons";
 import { OTPInput, OTPInputContext } from "input-otp";
-
 import { cn } from "@/lib/utils";
 
 const InputOTP = React.forwardRef<
