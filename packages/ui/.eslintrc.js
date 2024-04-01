5c76155eeaf1ba5128e3b8d3eec122cb4a0df39a
Abdullah Elsayed <11673834+abdllahdev@users.noreply.github.com>
2024-04-01 11:13:22 +0200

    fix(ui): fix ui config

--- packages/ui/.eslintrc.js
+++ packages/ui/.eslintrc.js
@@ -1,6 +1,7 @@
 module.exports = {
   root: true,
   extends: ["@repo/eslint-config/react-internal.js"],
+  ignorePatterns: ["tailwind.config.ts", "postcss.config.js"],
   rules: {
     "@typescript-eslint/explicit-function-return-type": "off",
     "@typescript-eslint/no-shadow": "off",
