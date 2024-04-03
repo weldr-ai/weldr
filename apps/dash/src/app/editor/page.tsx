f849e43dca6ea8eaa64b84afbc8feb9c257f1a2c
Abdullah Elsayed <11673834+abdllahdev@users.noreply.github.com>
2024-04-03 14:19:02 +0200

    refactor(dash): update dashboard

--- apps/dash/src/app/editor/page.tsx
+++ apps/dash/src/app/editor/page.tsx
@@ -1,3 +1,9 @@
 export default function Editor(): JSX.Element {
-  return <div />;
+  return (
+    <main className="flex size-full flex-col gap-4 p-4 md:gap-8 md:p-8">
+      <div className="flex items-center justify-center">
+        <div>Editor</div>
+      </div>
+    </main>
+  );
 }
