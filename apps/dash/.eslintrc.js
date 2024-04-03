c889d20ea0c803d29d8d9e64a3163d97dc6327eb
Abdullah Elsayed <11673834+abdllahdev@users.noreply.github.com>
2024-04-03 14:40:14 +0200

    style: add jsdocs to eslint config files

--- apps/dash/.eslintrc.js
+++ apps/dash/.eslintrc.js
@@ -1,3 +1,5 @@
+// This configuration only applies to the package manager root.
+/** @type {import("eslint").Linter.Config} */
 module.exports = {
   root: true,
   extends: ["@repo/eslint-config/next.js"],
