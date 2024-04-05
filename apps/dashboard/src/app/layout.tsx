c8c51e6e781628c01b73a1c3c47f301bd1de4421
Abdullah Elsayed <11673834+abdllahdev@users.noreply.github.com>
2024-04-05 02:46:34 +0200

    refactor: monorepo to the max babyyyyy

---
+++ apps/dashboard/src/app/layout.tsx
@@ -0,0 +1,45 @@
+import "@integramind/ui/globals.css";
+
+import type { Metadata } from "next";
+import { Poppins } from "next/font/google";
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
+
+import { ThemeProvider } from "@integramind/ui/theme-provider";
+import { cn } from "@integramind/ui/utils";
+
+import { Navbar } from "../components/navbar";
+
+const poppins = Poppins({ weight: "400", subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "IntegraMind",
+  description:
+    "Build LLM-powered AI agents in minutes for seamless workflow automation!",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}): JSX.Element {
+  return (
+    <html className="dark" lang="en">
+      <body
+        className={cn("flex min-h-screen w-full flex-col", poppins.className)}
+      >
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          disableTransitionOnChange
+          enableSystem
+        >
+          <Navbar />
+          {children}
+          <Analytics />
+          <SpeedInsights />
+        </ThemeProvider>
+      </body>
+    </html>
+  );
+}
