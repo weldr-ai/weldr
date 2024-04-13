8b44122dfcc2f1b18e9bafe40cb2b54570ce1120
Abdullah Elsayed <11673834+abdllahdev@users.noreply.github.com>
2024-04-13 05:00:49 +0200

    refactor: update ui theme and colors

--- tooling/tailwind/index.ts
+++ tooling/tailwind/index.ts
@@ -1,9 +1,17 @@
 import type { Config } from "tailwindcss";
+import aspectRatio from "@tailwindcss/aspect-ratio";
+import forms from "@tailwindcss/forms";
+import typography from "@tailwindcss/typography";
 import animate from "tailwindcss-animate";
+import { createPlugin } from "windy-radix-palette";
+import windyTypography from "windy-radix-typography";
+
+const colors = createPlugin();
 
 export default {
   darkMode: ["class"],
   content: ["src/**/*.{ts,tsx}"],
+  presets: [windyTypography],
   theme: {
     container: {
       center: true,
@@ -14,47 +22,145 @@ export default {
     },
     extend: {
       colors: {
-        border: "hsl(var(--border))",
-        input: "hsl(var(--input))",
-        ring: "hsl(var(--ring))",
-        background: "hsl(var(--background))",
-        foreground: "hsl(var(--foreground))",
-        primary: {
-          DEFAULT: "hsl(var(--primary))",
-          foreground: "hsl(var(--primary-foreground))",
-        },
-        secondary: {
-          DEFAULT: "hsl(var(--secondary))",
-          foreground: "hsl(var(--secondary-foreground))",
-        },
-        destructive: {
-          DEFAULT: "hsl(var(--destructive))",
-          foreground: "hsl(var(--destructive-foreground))",
-        },
+        background: colors.alias("slate.1"),
+        "subtle-background": colors.alias("slate.2"),
+        foreground: colors.alias("slate.12"),
+        "subtle-foreground": colors.alias("slate.11"),
+
         muted: {
-          DEFAULT: "hsl(var(--muted))",
-          foreground: "hsl(var(--muted-foreground))",
+          DEFAULT: colors.alias("slate.2"),
+          foreground: colors.alias("slate.11"),
         },
+
         accent: {
-          DEFAULT: "hsl(var(--accent))",
-          foreground: "hsl(var(--accent-foreground))",
+          DEFAULT: colors.alias("slate.4"),
+          foreground: colors.alias("slate.12"),
         },
-        popover: {
-          DEFAULT: "hsl(var(--popover))",
-          foreground: "hsl(var(--popover-foreground))",
-        },
+
         card: {
-          DEFAULT: "hsl(var(--card))",
-          foreground: "hsl(var(--card-foreground))",
+          DEFAULT: colors.alias({
+            light: "slate.1",
+            dark: "slate.2",
+          }),
+          foreground: colors.alias("slate.12"),
         },
-      },
-      borderColor: {
-        DEFAULT: "hsl(var(--border))",
+
+        popover: {
+          DEFAULT: colors.alias({
+            light: "slate.1",
+            dark: "slate.2",
+          }),
+          foreground: colors.alias("slate.12"),
+        },
+
+        input: colors.alias("slate.7"),
+        border: colors.alias("slate.7"),
+        ring: colors.alias("indigo.7"),
+
+        slate: {
+          1: colors.alias("slate.1"),
+          2: colors.alias("slate.2"),
+          3: colors.alias("slate.3"),
+          4: colors.alias("slate.4"),
+          5: colors.alias("slate.5"),
+          6: colors.alias("slate.6"),
+          7: colors.alias("slate.7"),
+          8: colors.alias("slate.8"),
+          9: colors.alias("slate.9"),
+          10: colors.alias("slate.10"),
+          11: colors.alias("slate.11"),
+          12: colors.alias("slate.12"),
+        },
+
+        primary: {
+          DEFAULT: colors.alias("indigo.9"),
+          foreground: colors.alias("indigo.12"),
+          1: colors.alias("indigo.1"),
+          2: colors.alias("indigo.2"),
+          3: colors.alias("indigo.3"),
+          4: colors.alias("indigo.4"),
+          5: colors.alias("indigo.5"),
+          6: colors.alias("indigo.6"),
+          7: colors.alias("indigo.7"),
+          8: colors.alias("indigo.8"),
+          9: colors.alias("indigo.9"),
+          10: colors.alias("indigo.10"),
+          11: colors.alias("indigo.11"),
+          12: colors.alias("indigo.12"),
+        },
+
+        secondary: {
+          DEFAULT: colors.alias("mint.9"),
+          foreground: colors.alias("mint.1"),
+          1: colors.alias("mint.1"),
+          2: colors.alias("mint.2"),
+          3: colors.alias("mint.3"),
+          4: colors.alias("mint.4"),
+          5: colors.alias("mint.5"),
+          6: colors.alias("mint.6"),
+          7: colors.alias("mint.7"),
+          8: colors.alias("mint.8"),
+          9: colors.alias("mint.9"),
+          10: colors.alias("mint.10"),
+          11: colors.alias("mint.11"),
+          12: colors.alias("mint.12"),
+        },
+
+        destructive: {
+          DEFAULT: colors.alias("ruby.9"),
+          foreground: colors.alias("ruby.12"),
+          1: colors.alias("ruby.1"),
+          2: colors.alias("ruby.2"),
+          3: colors.alias("ruby.3"),
+          4: colors.alias("ruby.4"),
+          5: colors.alias("ruby.5"),
+          6: colors.alias("ruby.6"),
+          7: colors.alias("ruby.7"),
+          8: colors.alias("ruby.8"),
+          9: colors.alias("ruby.9"),
+          10: colors.alias("ruby.10"),
+          11: colors.alias("ruby.11"),
+          12: colors.alias("ruby.12"),
+        },
+
+        warning: {
+          DEFAULT: colors.alias("yellow.9"),
+          foreground: colors.alias("yellow.12"),
+          1: colors.alias("yellow.1"),
+          2: colors.alias("yellow.2"),
+          3: colors.alias("yellow.3"),
+          4: colors.alias("yellow.4"),
+          5: colors.alias("yellow.5"),
+          6: colors.alias("yellow.6"),
+          7: colors.alias("yellow.7"),
+          8: colors.alias("yellow.8"),
+          9: colors.alias("yellow.9"),
+          10: colors.alias("yellow.10"),
+          11: colors.alias("yellow.11"),
+          12: colors.alias("yellow.12"),
+        },
+
+        success: {
+          DEFAULT: colors.alias("jade.9"),
+          foreground: colors.alias("jade.12"),
+          1: colors.alias("jade.1"),
+          2: colors.alias("jade.2"),
+          3: colors.alias("jade.3"),
+          4: colors.alias("jade.4"),
+          5: colors.alias("jade.5"),
+          6: colors.alias("jade.6"),
+          7: colors.alias("jade.7"),
+          8: colors.alias("jade.8"),
+          9: colors.alias("jade.9"),
+          10: colors.alias("jade.10"),
+          11: colors.alias("jade.11"),
+          12: colors.alias("jade.12"),
+        },
       },
       borderRadius: {
-        lg: "var(--radius)",
-        md: "calc(var(--radius) - 2px)",
-        sm: "calc(var(--radius) - 4px)",
+        lg: "0.5rem",
+        md: "calc(0.5rem - 2px)",
+        sm: "calc(0.5rem - 4px)",
       },
       keyframes: {
         "accordion-down": {
@@ -72,5 +178,5 @@ export default {
       },
     },
   },
-  plugins: [animate],
+  plugins: [animate, aspectRatio, forms, typography, colors.plugin],
 } satisfies Config;
